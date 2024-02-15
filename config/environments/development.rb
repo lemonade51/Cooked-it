57b2a81e7559012a684db259e10fb804e7b90025
EC2 Default User <ec2-user@ip-172-31-11-247.ap-northeast-1.compute.internal>
2024-02-15 14:41:32 +0000

    Add index

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "d9c5d5684d0a401fa2f27cd174fbf7d8.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
